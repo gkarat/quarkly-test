bcff5331f321edf96134137d1dde959ad572d2a0
Georgy Karataev <georgkarat@gmail.com>
2020-11-16 11:50:52 +0100

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,110 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb253e62c4ef2001e325cd2",
+				"5fb253e62c4ef2001e325cdc",
+				"5fb253e62c4ef2001e325cd4",
+				"5fb253e62c4ef2001e325cd6",
+				"5fb253e62c4ef2001e325cd8",
+				"5fb253e62c4ef2001e325cda",
+				"5fb253e62c4ef2001e325cd0",
+				"5fb253e62c4ef2001e325cde",
+				"5fb253e62c4ef2001e325ce0"
+			]
+		},
+		"5fb253e62c4ef2001e325cd0": {
+			"id": "5fb253e62c4ef2001e325cd0",
+			"name": "404",
+			"pageUrl": "404",
+			"styles": {
+				"background": "--color-darkL2"
+			},
+			"seo": {
+				"title": "404 | Onboarding Quarkly",
+				"og:title": "404 | Onboarding Quarkly"
+			}
+		},
+		"5fb253e62c4ef2001e325cd2": {
+			"id": "5fb253e62c4ef2001e325cd2",
+			"name": "Intro",
+			"pageUrl": "index",
+			"seo": {
+				"title": "Intro | Onboarding Quarkly",
+				"description": "At this stage, you are going to get to know the Quarkly interface and main features.\n",
+				"og:description": "At this stage, you are going to get to know the Quarkly interface and main features.\n",
+				"og:title": "Intro | Onboarding Quarkly"
+			}
+		},
+		"5fb253e62c4ef2001e325cd4": {
+			"id": "5fb253e62c4ef2001e325cd4",
+			"pageUrl": "stage-2-elementary",
+			"name": "Stage 2. Hover & Adaptive",
+			"seo": {
+				"title": "Hover & Adaptive | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn how to change styles for the hover state and mobile devices.\n",
+				"og:description": "At this stage, you're going to learn how to change styles for the hover state and mobile devices.\n",
+				"og:title": "Hover & Adaptive | Onboarding Quarkly"
+			}
+		},
+		"5fb253e62c4ef2001e325cd6": {
+			"id": "5fb253e62c4ef2001e325cd6",
+			"pageUrl": "stage-3-components",
+			"name": "Stage 3. Components",
+			"seo": {
+				"title": "Components | Onboarding Quarkly",
+				"og:title": "Components | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn how to create custom components, update and detach them, and reset changes.\n",
+				"og:description": "At this stage, you're going to learn how to create custom components, update and detach them, and reset changes.\n"
+			}
+		},
+		"5fb253e62c4ef2001e325cd8": {
+			"id": "5fb253e62c4ef2001e325cd8",
+			"pageUrl": "stage-4-theme",
+			"name": "Stage 4. Theme",
+			"seo": {
+				"title": "Theme | Onboarding Quarkly",
+				"og:title": "Theme | Onboarding Quarkly",
+				"description": "At this stage, you're going to get to know themes, learn how to ass variables and new breakpoints to it.\n",
+				"og:description": "At this stage, you're going to get to know themes, learn how to ass variables and new breakpoints to it.\n"
+			}
+		},
+		"5fb253e62c4ef2001e325cda": {
+			"id": "5fb253e62c4ef2001e325cda",
+			"pageUrl": "stage-5-code-editor",
+			"name": "Stage 5. Code Editor",
+			"seo": {
+				"title": "Code Editor | Onboarding Quarkly",
+				"og:title": "Code Editor | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn the basics of working in the code editor.",
+				"og:description": "At this stage, you're going to learn the basics of working in the code editor."
+			}
+		},
+		"5fb253e62c4ef2001e325cdc": {
+			"id": "5fb253e62c4ef2001e325cdc",
+			"pageUrl": "stage-1-basics",
+			"name": "Stage 1. Basics",
+			"seo": {
+				"title": "Basics | Onboarding Quarkly",
+				"description": "At this stage, you're going to learn how to add elements to the page, edit their content and style properties.\n",
+				"og:description": "At this stage, you're going to learn how to add elements to the page, edit their content and style properties.\n",
+				"og:title": "Basics | Onboarding Quarkly"
+			}
+		},
+		"5fb253e62c4ef2001e325cde": {
+			"id": "5fb253e62c4ef2001e325cde",
+			"pageUrl": "parrent-page",
+			"name": "Parent page"
+		},
+		"5fb253e62c4ef2001e325ce0": {
+			"id": "5fb253e62c4ef2001e325ce0",
+			"pageUrl": "children-page",
+			"name": "Children page"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
